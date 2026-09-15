227f29a182197be24346ebe77f5388ce2d85d5ac
Akshara Bhardwaj <aksharabhardwaj766@gmail.com>
2026-09-15 11:22:57 +0530

    Add_forward_returns: Multi-horizon forward return labeling via merge_asof

---
+++ data_labels.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def add_forward_returns(df: pd.DataFrame, horizons:list[float]) -> pd.DataFrame:
+    df = df.sort_values('time').reset_index(drop=True)
+
+    for h in horizons:
+        future_df = df[['time', 'mid_price']].copy()
+        future_df = future_df.rename(columns={'mid_price': 'mid_price_future'})
+        future_df['time'] = future_df['time'] - h
+
+        merged = pd.merge_asof(
+            df.sort_values('time'),
+            future_df.sort_values('time'),
+            on='time',
+            direction='forward'
+        )
+
+        df[f'fwd_returns_{h}s'] = merged['mid_price_future'] - merged['mid_price']
+
+        pass
+    return df  
